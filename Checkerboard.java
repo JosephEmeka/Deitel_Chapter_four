96be4f7533c579aa9c2c735ba34024f84b6db089
Joseph <josephemekaonah@gmail.com>
2023-12-15 17:56:29 +0100

    Checkerboard.java

---
+++ Checkerboard.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class Checkerboard{
+    
+	public static void main(String[] args) {
+        
+	Scanner input = new Scanner(System.in);
+
+
+	for (int item = 0; item < 4; item ++) {
+
+		System.out.println("* * * * * * * * "); 
+		System.out.println();
+		System.out.println(" * * * * * * * * ");
+		System.out.println();
+		
+
+	}
+
+}
+
+}
