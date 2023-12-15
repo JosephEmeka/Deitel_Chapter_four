c6635e1267094b0dc32afe35013357858702d2ae
Joseph <josephemekaonah@gmail.com>
2023-12-15 18:08:12 +0100

    LargestNumberFinder.java

---
+++ LargestNumberFinder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LargestNumberFinder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int counter = 1;
+        int largest = 0;
+
+        while (counter <= 10) {
+            System.out.print("Enter an integer: ");
+            int number = input.nextInt();
+            
+            if (number > largest) {
+                largest = number;
+            }
+            
+            counter++;
+        }
+
+        System.out.println("The largest number is: " + largest);
+        
+    }
+}
